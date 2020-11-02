c76538b0db4bc5474d52152dbee930859efa71ba
rukkiechovwe <rukkiechovwe@gmail.com>
2020-11-02 16:26:28 +0100

    Updates

---
+++ precache-manifest.ec49767046f75bda4aada823da2921a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bd823a10a14748235e19cfe6e24fb6",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "a50dbddaa1521c56d6d3",
+    "url": "/shopping-cart/static/css/main.b54779d1.chunk.css"
+  },
+  {
+    "revision": "2c6d5540545cafa8120d",
+    "url": "/shopping-cart/static/js/2.c9065ec1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart/static/js/2.c9065ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50dbddaa1521c56d6d3",
+    "url": "/shopping-cart/static/js/main.7b495cd2.chunk.js"
+  },
+  {
+    "revision": "8da712e246551231155d",
+    "url": "/shopping-cart/static/js/runtime-main.2f838fe6.js"
+  }
+]);
